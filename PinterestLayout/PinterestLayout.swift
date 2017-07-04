be746a297ce8a98b3bbe3064696d9ebfd60c368c
Khrystyna Shevchuk <khrystyna.shevchuk94@gmail.com>
2017-07-04 20:32:46 +0300

    - added layouts for cells - added collection view & PinterestVC - added images

--- PinterestLayout/PinterestLayout.swift
+++ PinterestLayout/PinterestLayout.swift
@@ -9,6 +9,131 @@
 import UIKit
 
 
+//MARK: CollectionViewLayoutDelegate
+
+public protocol CollectionViewLayoutDelegate {
+    
+    func collectionView(collectionView: UICollectionView, heightForImageAtIndexPath indexPath: NSIndexPath, withWidth: CGFloat) -> CGFloat
+    
+    func collectionView(collectionView: UICollectionView, heightForAnnotationAtIndexPath indexPath: NSIndexPath, withWidth: CGFloat) -> CGFloat
+}
+
+
+//MARK: CollectionViewLayoutAttributes
+
+public class CollectionViewLayoutAttributes: UICollectionViewLayoutAttributes {
+    
+    public var imageHeight: CGFloat = 0
+    
+    override public func copy(with zone: NSZone? = nil) -> Any {
+        let copy = super.copy(with: zone) as! CollectionViewLayoutAttributes
+        copy.imageHeight = imageHeight
+        return copy
+    }
+    
+    override public func isEqual(_ object: Any?) -> Bool {
+        if let attributes = object as? CollectionViewLayoutAttributes {
+            if attributes.imageHeight == imageHeight {
+                return super.isEqual(object)
+            }
+        }
+        return false
+    }
+}
+
+
+//MARK: CollectionViewLayout
+
 public class PinterestLayout: UICollectionViewLayout {
     
+    public var delegate: CollectionViewLayoutDelegate!
+    public var numberOfColumns: Int = 1
+    public var cellPadding: CGFloat = 0
+    public var numberOfSections: Int = 1
+    var insets = UIEdgeInsets.zero
+    
+    private var cache = [CollectionViewLayoutAttributes]()
+    private var contentHeight: CGFloat = 0
+    private var width: CGFloat {
+        get {
+            insets = collectionView!.contentInset
+            let bounds = collectionView!.bounds
+            return bounds.width - insets.left - insets.right
+        }
+    }
+    
+    override public var collectionViewContentSize: CGSize {
+        get {
+            return CGSize(width: width, height: contentHeight)
+        }
+    }
+    
+    override public class var layoutAttributesClass: AnyClass {
+        return CollectionViewLayoutAttributes.self
+    }
+    
+    override public func prepare() {
+        
+        if cache.isEmpty {
+            let collumnWidth = width / CGFloat(numberOfColumns)
+            let cellWidth = collumnWidth - (cellPadding * 2)
+            
+            var xOffsets = [CGFloat]()
+            
+            for collumn in 0..<numberOfColumns {
+                xOffsets.append(CGFloat(collumn) * collumnWidth)
+            }
+            
+            var yOffsets = [CGFloat](repeating: 0, count: numberOfColumns)
+            for section in 0..<numberOfSections {
+                
+                var column = 0
+                var oneColumn = Int(collectionView!.numberOfItems(inSection: section)/numberOfColumns)
+                if collectionView!.numberOfItems(inSection: section) % numberOfColumns != 0 {
+                    oneColumn += 1
+                }
+                for item in 0..<collectionView!.numberOfItems(inSection: section) {
+                    
+                    let indexPath = IndexPath(item: item, section: section)
+                    
+                    column = Int(item/(oneColumn))
+                    
+                    let imageHeight = delegate.collectionView(collectionView: collectionView!, heightForImageAtIndexPath: indexPath as NSIndexPath, withWidth: cellWidth)
+                    let annotationHeight = delegate.collectionView(collectionView: collectionView!, heightForAnnotationAtIndexPath: indexPath as NSIndexPath, withWidth: cellWidth)
+                    let cellHeight = cellPadding + imageHeight + annotationHeight + cellPadding
+                    
+                    let frame = CGRect(
+                        x: xOffsets[column],
+                        y: yOffsets[column],
+                        width: collumnWidth,
+                        height: cellHeight)
+                    let insetFrame = UIEdgeInsetsInsetRect(frame, insets)
+                    
+                    let attributes = CollectionViewLayoutAttributes(
+                        forCellWith: indexPath
+                    )
+                    attributes.frame = insetFrame
+                    attributes.imageHeight = imageHeight
+                    cache.append(attributes)
+                    
+                    let cgrectFrame = frame
+                    contentHeight = max(contentHeight, cgrectFrame.maxY)
+                    yOffsets[column] = yOffsets[column] + cellHeight
+                }
+            }
+        }
+    }
+    
+    override public func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        
+        var layoutAttributes = [UICollectionViewLayoutAttributes]()
+        
+        for attributes in cache {
+            if attributes.frame.intersects(rect) {
+                layoutAttributes.append(attributes)
+            }
+        }
+        
+        return layoutAttributes
+    }
 }
---
+++ PinterestLayoutExample/PinterestVC.swift
@@ -0,0 +1,87 @@
+//
+//  PinterestVC.swift
+//  PinterestLayoutExample
+//
+//  Created by Roman Sorochak on 7/4/17.
+//  Copyright © 2017 MagicLab. All rights reserved.
+//
+
+import UIKit
+import PinterestLayout
+import AVFoundation
+
+
+class PinterestVC: UICollectionViewController {
+    
+    var images: [[UIImage]] = [[#imageLiteral(resourceName: "new_york"), #imageLiteral(resourceName: "bigben_river"), #imageLiteral(resourceName: "dubai"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "tiger"), #imageLiteral(resourceName: "5")], [#imageLiteral(resourceName: "adelaide_castle"), #imageLiteral(resourceName: "rys"), #imageLiteral(resourceName: "12"), #imageLiteral(resourceName: "cat"), #imageLiteral(resourceName: "6"), #imageLiteral(resourceName: "city"), #imageLiteral(resourceName: "9"), #imageLiteral(resourceName: "ph"), #imageLiteral(resourceName: "paris"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "bridge"), #imageLiteral(resourceName: "elephants")], [#imageLiteral(resourceName: "venice"), #imageLiteral(resourceName: "bw_new_york"), #imageLiteral(resourceName: "10"), #imageLiteral(resourceName: "kittens"), #imageLiteral(resourceName: "deer"), #imageLiteral(resourceName: "11"), #imageLiteral(resourceName: "8"), #imageLiteral(resourceName: "13"), #imageLiteral(resourceName: "squirell"), #imageLiteral(resourceName: "lion"), #imageLiteral(resourceName: "nature"), #imageLiteral(resourceName: "4")]]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCollectionViewInsets()
+        setupLayout()
+    }
+    
+    
+    //MARK: private
+    
+    private func setupCollectionViewInsets() {
+        collectionView!.backgroundColor = .clear
+        collectionView!.contentInset = UIEdgeInsets(top: 15, left: 5, bottom: 5, right: 5)
+    }
+    
+    private func setupLayout() {
+        guard let layout = collectionViewLayout as? PinterestLayout else { return }
+        layout.delegate = self
+        layout.cellPadding = 5
+        layout.numberOfColumns = 2
+        layout.numberOfSections = images.count
+    }
+}
+
+
+//MARK: UICollectionViewDataSource
+
+extension PinterestVC {
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images[section].count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let image = images[indexPath.section][indexPath.item]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath) as! CollectionViewCell
+        cell.image = image
+        
+        // bad way of setting text, but just for a test
+        if indexPath.item % 2 == 0 {
+            cell.descriptionLabel.text = "Very short description"
+        } else {
+            cell.descriptionLabel.text = "Tonight, there are no lovers walking down the park alleys. There are no kings in the castles, and the princesses die alone – they have no frogs or peas. Tonight, the inkpots are empty, and the words are uncountable. Tonight, all shouts are muffled by unbearable silence."
+        }
+        
+        return cell
+    }
+}
+
+
+//MARK: CollectionViewLayoutDelegate
+
+extension PinterestVC: CollectionViewLayoutDelegate {
+    func collectionView(collectionView: UICollectionView, heightForImageAtIndexPath indexPath: NSIndexPath, withWidth: CGFloat) -> CGFloat {
+        
+        let image = images[indexPath.section][indexPath.item]
+        let boundingRect = CGRect(x: 0, y: 0, width: withWidth, height: CGFloat(MAXFLOAT))
+        let rect = AVMakeRect(aspectRatio: image.size, insideRect: boundingRect)
+        return rect.height
+    }
+    
+    func collectionView(collectionView: UICollectionView, heightForAnnotationAtIndexPath indexPath: NSIndexPath, withWidth: CGFloat) -> CGFloat {
+        return 50
+    }
+}
