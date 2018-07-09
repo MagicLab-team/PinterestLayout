0f6906c3c4b81001dc5afcbafd0c344eef3be847
Dennis Parussini <thedan84@mac.com>
2018-07-09 09:01:42 +0200

    Updated to Xcode 9 and Swift 4

--- PinterestLayout/Help.swift
+++ PinterestLayout/Help.swift
@@ -38,7 +38,7 @@ public extension String {
         let rect = NSString(string: self).boundingRect(
             with: CGSize(width: width, height: CGFloat(MAXFLOAT)),
             options: .usesLineFragmentOrigin,
-            attributes: [NSFontAttributeName: font],
+            attributes: [NSAttributedStringKey.font: font],
             context: nil
         )
         return ceil(rect.height)
